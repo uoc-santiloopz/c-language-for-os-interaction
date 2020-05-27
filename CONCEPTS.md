abcf9e62089b7f21f938d207b9b8ce513a61fbd2
thir13en <santilpzate@gmail.com>
2020-05-28 01:40:09 +0200

    added concepts file

---
+++ CONCEPTS.md
@@ -0,0 +1,3 @@
+# Concepts
+
+* C preprocessor (cpp)
--- src/hello-world.c
+++ src/hello-world.c
@@ -1,12 +1,16 @@
+// all the lines that start with a hash are treated by the preprocessor (cpp)
+// to include header files such as this ones, macros extension or conditional rendering
 #include <unistd.h>
 #include <stdio.h>
 #include <string.h>
 
 
-int main()
+int main(void)
 {
 	char c[19];
 
 	sprintf(c, "Hello World!\n");
 	write(1, c, strlen(c));
+
+	return 0;
 }
